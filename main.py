be26fa43e798a457ea997468dce90e9f9bed9b57
Caio Magalhães <69921724+ravencaio@users.noreply.github.com>
2025-09-06 16:13:21 +0000

    test

--- main.py
+++ main.py
@@ -1,3 +1,2 @@
 from utils import player, ui
 
-player.printar()
---
+++ utils/player/__init__.py
@@ -0,0 +1,9 @@
+import playsound
+
+class Music:
+    def __init__(self, title, duration, file):
+        self.title = title
+        self.duration = duration
+        self.file = file
+
+    
