8cd34e3c9947b3128dd8435d5288f11035d92843
Caio Magalhães <69921724+ravencaio@users.noreply.github.com>
2025-09-11 22:03:54 +0000

    new_classes

--- utils/player/__init__.py
+++ utils/player/__init__.py
@@ -1,9 +1,25 @@
 import playsound
 
 class Music:
-    def __init__(self, title, duration, file):
+    def __init__(self, id, title, duration, file):
+        self.id = id
         self.title = title
         self.duration = duration
         self.file = file
+    def __str__(self):
+        return f'ID : {self.id} | Name: {self.title} | Duration: {self.duration} | File location: {self.file}'
+
+class MusicList:
+    def __init__(self, music_list = []):
+        self.music_list = music_list
+    
+    def add_music(self, music):
+        self.music_list.append(music)
+    
+    def play_music(self, id):
+        for music in self.music_list:
+            if music.id == id:
+                playsound
+
 
     
